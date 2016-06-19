38ccbe5f9b1ecd5f6c6e43997df773553434fde5
cex <cex@imac-de-cex.local>
2016-06-19 22:46:17 +0200

    Primera versión válida

--- HamburguesasEnElMundo/HamburguesasEnElMundo/Datos.swift
+++ HamburguesasEnElMundo/HamburguesasEnElMundo/Datos.swift
@@ -2,8 +2,75 @@
 //  Datos.swift
 //  HamburguesasEnElMundo
 //
-//  Created by cex on 19/6/16.
+//  Este archivo fuente contendrá las clases de datos usadas en la aplicación
+//
+//  Created by Adrián Rejas on 19/6/16.
 //
 //
 
 import Foundation
+import UIKit
+
+// Colección de paises que se usarán en la aplicación. Se ha testeado en un playground que hay 20.
+class ColeccionDePaises {
+    // Array de strings con los paises a usar; si se quiere incluir alguno añadirlo a este array.
+    let paises : [String] = ["España", "Inglaterra", "Francia", "Alemania", "Italia", "Polonia", "Holanda", "Bélgica", "Rusia", "Estados Unidos", "Canada", "Mexico", "Japón", "China", "Australia", "Brasil", "Argentina", "Colombia", "Marruecos", "Turquía"]
+    
+    /* OPINION PERSONAL: Esta función si de mi dependiese la declararía como función estática y la llamaría a través del nombre de la clase en vez del nombre de la instancia que se use. De esta manera no sería necesario crear una instancia de ColeccionDePaises para llamar a obtenPais(). Es más, usaría un enum con métodos estáticos declarados, creo que tiene más sentido. Pero como en el enunciado se dice claramente que tiene que ser así, así se hará. */
+    // Función para obtener el string que representa un país de forma aleatoria
+    func obtenPais() -> String {
+        let posicion = Int(arc4random())%paises.count
+        return paises[posicion]
+    }
+    
+}
+
+// Colección de hamburguesas que se usarán en la aplicación. Se ha testeado en un playground que hay 20.
+class ColeccionDeHamburguesas {
+    // Array de strings con las hamburguesas a usar; si se quiere incluir alguna añadirla a este array.
+    let hamburguesas : [String] = ["Hamburguesa sencilla", "Hamburguesa con queso", "Hamburguesa con bacon y queso", "Hamburguesa con bacon, queso y huevo", "Hamburguesa Doble", "Hamburguesa doble con queso", "Hamburguesa doble con bacon y queso", "Hamburguesa doble con bacon,, queso y huevo", "Hamburguesa con vegetales", "Hamburguesa con vegetales y queso", "Hamburguesa con vegetales, bacon y queso", "Hamburguesa con vegetales, bacon, queso y huevo", "Hamburguesa especial con jalapeños", "Hamburguesa especial 4 quesos", "Hamburguesa especial con setas", "Hamburguesa especial con ternasco", "Hamburguesa de pollo", "Hamburguesa de pollo con queso", "Hamburguesa de pollo con bacon y queso", "Hamburguesa vegana"]
+    
+    // OPCIONAL A LA TAREA: estas variables definen las constantes a usar para calcular el precio de la hamburguesa.
+    let precioMinimo : Float = 3.0
+    let precioVariacion : Float = 0.10
+    
+    /* OPINION PERSONAL: Lo comentado en obtenPais() se aplica también en esta función */
+    // Función para obtener el string que representa una hamburguesa de forma aleatoria
+    func obtenHamburguesa() -> String {
+        let posicion = Int(arc4random())%hamburguesas.count
+        return hamburguesas[posicion]
+    }
+    
+    // OPCIONAL A LA TAREA: Aunque en las instrucciones no se habla de ello, como en la descripción de la tarea se habla de un precio he decidido añadir el precio de forma extra a la aplicación. Lo normal sería hacer un array bidimensional en el que se especificase el precio de cada hamburguesa en cada país. Pero como se trata de un añadido extra para la tarea y no es necesario complicar el programa en exceso más alla de la tarea, he decidico que el precio de cada hamburguesa en cada país será igua a (precioMinimo + (longitudStringHamburguesa+longitudStringPais)*precioVariacion
+    func obtenPrecioHamburguesaPorPais(hamburguesa : String, pais : String) -> Float{
+        return (precioMinimo + (Float(hamburguesa.characters.count + pais.characters.count)*precioVariacion))
+    }
+    
+}
+
+// Estructura representando los colores que puede tomar el fondo de la aplicación. Es la estructura usada por el profesor en los video del curso.
+struct Colores {
+    // Listado de colores
+    let colores = [ UIColor(red:210/255.0, green: 90/255.0, blue: 45/255.0, alpha: 0.5),
+        
+        UIColor(red:40/255.0, green: 170/255.0, blue: 45/255.0, alpha: 0.5),
+        
+        UIColor(red:3/255.0, green: 180/255.0, blue: 90/255.0, alpha: 0.5),
+        
+        UIColor(red:210/255.0, green: 190/255.0, blue: 5/255.0, alpha: 0.5),
+        
+        UIColor(red:120/255.0, green: 120/255.0, blue: 50/255.0, alpha: 0.5),
+        
+        UIColor(red:130/255.0, green: 80/255.0, blue: 90/255.0, alpha: 0.5),
+        
+        UIColor(red:130/255.0, green: 130/255.0, blue: 130/255.0, alpha: 0.5),
+        
+        UIColor(red:3/255.0, green: 50/255.0, blue: 90/255.0, alpha: 0.5)]
+    
+    // Función para obtener un color aleatorio
+    func regresaColorAleatorio() -> UIColor{
+        let posicion = Int(arc4random())%colores.count
+        return colores[posicion]
+    }
+    
+}
--- HamburguesasEnElMundo/HamburguesasEnElMundo/ViewController.swift
+++ HamburguesasEnElMundo/HamburguesasEnElMundo/ViewController.swift
@@ -2,14 +2,26 @@
 //  ViewController.swift
 //  HamburguesasEnElMundo
 //
-//  Created by cex on 19/6/16.
+//  En el ViewController implementaremos la lógica de control de la interfaz gráfica para que cuando se pulse el botón (centrado horizontalmente y ajustado abajo) que está en la parte baja de la pantalla se actualizen tres etiquetas (centradas horizontal y verticalmente) con la información de hamburguesa, país y precio (las dos primeras solicitadas por el enunciado de la aplicación, la segunda es opcional) se actualicen con valores aleatorios.
+//
+//  Created by Adrián Rejas on 19/6/16.
 //
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Instancias de las colecciones de paises, hamburguesas y colores de los datos de swift. Datos provenientes de Datos.swift
+    let paises = ColeccionDePaises()
+    let hamburguesas = ColeccionDeHamburguesas()
+    let colores = Colores()
 
+    // IBOutlets relacionados con las UILabels usadas para mostrar la información de país, hamburguesa y precio.
+    @IBOutlet weak var paisLabel: UILabel!
+    @IBOutlet weak var hamburguesaLabel: UILabel!
+    @IBOutlet weak var precioLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func mostrarNuevaHamburguesa() {
+        // Primero realizamos una comprobación de que las etiquetas están instanciadas, para evitar errores de ejecución
+        if paisLabel == nil || hamburguesaLabel == nil || precioLabel == nil {
+            return
+        }
+        // Ahora se obtiene el país y la hamburguesa a mostrar aleatoriamente, y el precio correspondiente a dicha pareja
+        let pais : String = paises.obtenPais()
+        let hamburguesa : String = hamburguesas.obtenHamburguesa()
+        let precio : Float = hamburguesas.obtenPrecioHamburguesaPorPais(hamburguesa, pais: pais)
+        // Actualizamos las etiquetas con la información recién obtenida
+        paisLabel.text = "Pais: \(pais)"
+        hamburguesaLabel.text = "Hamburguesa: \(hamburguesa)"
+        precioLabel.text = "Precio: \(precio) $"
+        //OPCIONAL A LA TAREA: Cambiamos el color del fondo y del tilt aleatoriamente
+        let color = colores.regresaColorAleatorio()
+        view.backgroundColor = color
+        view.tintColor = color
+        // OPCIONAL A LA TAREA: Por defecto he puesto el alpha de las etiquetas a 0.3, para recalcar que al principio no hay información valida. En cuanto se pulsa el botón, se ponte el alpha de estas estiquetas  a 1.
+        paisLabel.alpha=1
+        hamburguesaLabel.alpha=1
+        precioLabel.alpha=1
+    }
 
 }
 
